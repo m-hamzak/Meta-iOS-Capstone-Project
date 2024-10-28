d14e7d7195979b4b40fdcadd320563c6496992cb
Muhammad Hamza Khalid <46857881+m-hamzak@users.noreply.github.com>
2024-10-28 17:06:12 +0500

    Add files via upload
    Added project

---
+++ LittleLemonApp/LittleLemonApp/Views/Home.swift
@@ -0,0 +1,22 @@
+//
+//  Home.swift
+//  LittleLemonApp
+//
+//  Created by M Hamza Khalid on 28/10/2024
+//
+
+import SwiftUI
+
+struct Home: View {
+    
+    var body: some View {
+        MainScreen()
+            .navigationBarBackButtonHidden()
+    }
+}
+
+struct Home_Previews: PreviewProvider {
+    static var previews: some View {
+        Home().environment(\.managedObjectContext, PersistenceController.shared.container.viewContext)
+    }
+}
---
+++ LittleLemonApp/LittleLemonApp/Views/MainScreen.swift
@@ -0,0 +1,27 @@
+//
+//  MainScreen.swift
+//  LittleLemonApp
+//
+//   Created by M Hamza Khalid on 28/10/2024
+//
+
+import SwiftUI
+
+struct MainScreen: View {
+    @Environment(\.managedObjectContext) private var viewContext
+    
+    var body: some View {
+        NavigationStack {
+            VStack {
+                Header()
+                Menu()
+            }
+        }
+    }
+}
+
+struct MainScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        MainScreen().environment(\.managedObjectContext, PersistenceController.shared.container.viewContext)
+    }
+}
